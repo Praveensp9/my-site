60058f0ddf68a86dcbce2512a380460def2d952a
Praveen <prvnsp9@gmail.com>
2019-04-10 07:35:53 -0700

    Updates

--- precache-manifest.eab09cd7d3dd8c3b95d3d395d72bec8c.js
+++ precache-manifest.eab09cd7d3dd8c3b95d3d395d72bec8c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/my-site/static/js/runtime~main.0ae2f0e4.js"
   },
   {
-    "revision": "fb9f67c45aa6bf084e5e",
-    "url": "/my-site/static/js/main.e5490169.chunk.js"
+    "revision": "ca9354d4aed07cf0c4f3",
+    "url": "/my-site/static/js/main.0b3a6055.chunk.js"
   },
   {
     "revision": "2899c966e04b4feec520",
     "url": "/my-site/static/js/2.bb2b29d9.chunk.js"
   },
   {
-    "revision": "fb9f67c45aa6bf084e5e",
+    "revision": "ca9354d4aed07cf0c4f3",
     "url": "/my-site/static/css/main.07318036.chunk.css"
   },
   {
-    "revision": "3b803a1b3c0bb97018c4bc798f8a6a6d",
+    "revision": "0b4e171690ccb965ab02e1e42fabdf6d",
     "url": "/my-site/index.html"
   }
 ];

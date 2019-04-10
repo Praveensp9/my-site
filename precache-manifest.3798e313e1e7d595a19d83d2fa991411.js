e3c20b7cba43bc104e1240dce0476eca7bd1caf8
Praveen <prvnsp9@gmail.com>
2019-04-10 06:39:39 -0700

    Updates

---
+++ precache-manifest.3798e313e1e7d595a19d83d2fa991411.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32bfdd9f18f755e00cad",
+    "url": "/my-site/static/js/runtime~main.0ae2f0e4.js"
+  },
+  {
+    "revision": "53bc78494f9ffa7d6134",
+    "url": "/my-site/static/js/main.561c1cae.chunk.js"
+  },
+  {
+    "revision": "2899c966e04b4feec520",
+    "url": "/my-site/static/js/2.bb2b29d9.chunk.js"
+  },
+  {
+    "revision": "53bc78494f9ffa7d6134",
+    "url": "/my-site/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "96bc50f6f42f5c9958c518f0f802d8e5",
+    "url": "/my-site/index.html"
+  }
+];
